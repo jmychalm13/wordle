40d8b54fc18d93fe4c59841ae7349734325dac80
“Jessica <jmychalm13@gmail.com>
2023-07-08 19:12:58 -0500

    fully functional using SpicyProton

--- wordle.rb
+++ wordle.rb
@@ -3,7 +3,6 @@ system "clear"
 puts "Welcome to Wordle! Have fun"
 
 word = Spicy::Proton.noun(length: 5)
-pp word
 
 word_arr = word.split("")
 
@@ -31,11 +30,9 @@ until guesses == 0
   # comparing arrays and returning result as array
   index = 0
   5.times do
-    # todo: account for counting same letter twice HOW????
     if word_arr[index] == guess_arr[index]
       return_arr << "G"
     elsif word_arr.include?(guess_arr[index]) && word_arr.count(guess_arr[index]) > 1
-      # check count of letters against position
       return_arr << "Y"
     else
       return_arr << "R"
